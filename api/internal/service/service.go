1f04bcfff6b2db44a02e4efe42981d7f53bbd784
furrygem <85435415+furrygem@users.noreply.github.com>
2024-05-02 14:28:18 +0300

    service.go

---
+++ api/internal/service/service.go
@@ -0,0 +1,11 @@
+package service
+
+type DNSCrud struct{}
+
+func (dnscrud *DNSCrud) listRecords() {}
+
+func (dnscrud *DNSCrud) addRecord() {}
+
+func (dnscrud *DNSCrud) update_record() {}
+
+func (dnscrud *DNSCrud) delete_record() {}
